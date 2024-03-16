2d030a0c208d72c70b8fc49fed792cdd1c0e2712
neruneruna7 <113534341+neruneruna7@users.noreply.github.com>
2024-03-16 21:10:08 +0900

    prepare deploy

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -35,7 +35,7 @@ async fn main(
 
     // Get the discord token set in `Secrets.toml`
     let discord_token = secret_store
-        .get("DISCORD_TOKEN_TEST")
+        .get("DISCORD_TOKEN")
         .context("'DISCORD_TOKEN' was not found")?;
 
     use commands::{
