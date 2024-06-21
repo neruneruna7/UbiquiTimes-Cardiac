a4fbfc91cbfa8012b42d5303bf84440d85ec2f6c
neruneruna7 <neruneruna7@gmail.com>
2024-06-21 19:32:00 +0900

    update: onetimetoken

--- share/src/one_time_token.rs
+++ share/src/one_time_token.rs
@@ -1,4 +1,5 @@
 use jsonwebtoken::{encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
+use rand::Rng;
 use serde::{Deserialize, Serialize};
 use time::OffsetDateTime;
 
@@ -7,16 +8,26 @@ struct Claims {
     sub: String,
     iat: usize,
     exp: usize,
+    discord_user_id: Option<u64>,
+    slack_user_id: Option<String>,
 }
 
 impl Claims {
-    pub fn new(sub: String) -> Self {
+    fn new(sub: String, discord_user_id: Option<u64>, slack_user_id: Option<String>) -> Self {
         let now = OffsetDateTime::now_utc();
         // 1分間の有効期限
         let exp = now + time::Duration::minutes(1);
         let exp = exp.unix_timestamp() as usize;
         let iat = now.unix_timestamp() as usize;
-        Self { sub, iat, exp }
+        Self { sub, iat, exp, discord_user_id, slack_user_id }
+    }
+
+    pub fn new_on_discord(sub: String, discord_user_id: u64) -> Self {
+        Self::new(sub, Some(discord_user_id), None)
+    }
+
+    pub fn new_on_slack(sub: String, slack_user_id: String) -> Self {
+        Self::new(sub, None, Some(slack_user_id))
     }
 }
 
@@ -47,7 +58,7 @@ mod tests {
     #[test]
     fn test_encode_decode_jwt() {
         let secret = SECRET.to_string();
-        let claim = Claims::new("test".to_string());
+        let claim = Claims::new("test".to_string(), None, None);
         let token = encode_jwt(claim.clone(), secret.clone()).unwrap();
         let decoded_claim = decode_jwt(&token, secret).unwrap();
 
@@ -57,14 +68,17 @@ mod tests {
     #[test]
     fn test_expired_token() {
         let secret = SECRET.to_string();
-        let claim = Claims::new("test".to_string());
+        let claim = Claims::new("test".to_string(), None, None);
         let exp = OffsetDateTime::now_utc() - time::Duration::minutes(2);
         let exp = exp.unix_timestamp() as usize;
         let iat = OffsetDateTime::now_utc().unix_timestamp() as usize;
+
         let expired_claim = Claims {
             sub: claim.sub,
             iat,
             exp,
+            discord_user_id: claim.discord_user_id,
+            slack_user_id: claim.slack_user_id,
         };
         let token = encode_jwt(expired_claim, secret.clone()).unwrap();
         let result = decode_jwt(&token, secret);
